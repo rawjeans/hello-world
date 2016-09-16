22a41ff6829b3633c7e149cd9a3c5d97108f7143
Razeen <dubfan24@gmail.com>
2016-09-16 17:36:50 -0400

    C++ File
    Hello World in C++

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+/*
+ * hello.cpp
+ *  
+ * EECS Practice 
+ *
+ * Razeen Ghazi
+ * razeen@umich.edu
+ *
+ * Says hello to the world.
+ */
+
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello, world\n";
+    return 0;
+}
